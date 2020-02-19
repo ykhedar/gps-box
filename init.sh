a7d0760dd93eb1de3906073d611d1a491a2743fd
Yogesh Khedar <y.khedar@tu-braunschweig.de>
2020-02-19 14:55:19 +0100

    updated for rtk

--- init.sh
+++ init.sh
@@ -34,6 +34,6 @@ cd /home/debian/gps-box && python internet_check.py &
 # Start the git repo status script
 cd /home/debian/gps-box && python git_check.py &
 
-sleep 15
+#sleep 15
 
-cd /home/debian && sh start_str2str.sh
+#cd /home/debian && sh start_str2str.sh
