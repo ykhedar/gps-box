0f32004358ccf990d5936d927007cca684f42bdc
Yogesh Khedar <y.khedar@tu-braunschweig.de>
2020-08-20 13:21:32 +0200

    restart when no internet for 60 seconds.

--- internet_check.py
+++ internet_check.py
@@ -3,6 +3,8 @@
 import os
 import datetime
 import time
+from collections import deque
+
 
 MAIN_LOG_DIR="/home/debian/"
 CURRENT_DATE_TIME=datetime.datetime.now().isoformat().replace(":", "-")
@@ -16,6 +18,8 @@ if not os.path.exists(os.path.dirname(NET_LOG_DIR)):
 log_file = open(NET_LOG_FILE, "wa", buffering=1)
 internet_status = False
 vpn_status = False
+circular_buffer = deque(maxlen=60)
+
 
 while True:
 	internet_ping = os.popen('ping -c 1 -w 1 8.8.8.8').read().split('\n')
@@ -25,6 +29,11 @@ while True:
 		internet_status = str(bool(internet_ping[1]))
 	else:
 		internet_status = str(bool(False))
+	circular_buffer.append(internet_status)
+
+	if not all(list(circular_buffer)):
+		shutdow_result = os.popen("shutdown -r now").read()
+		log_file.write(datetime.datetime.now().isoformat() + "," + shutdow_result + "\n")
 
 	if len(vpn_ping) > 1:
 		vpn_status = str(bool(vpn_ping[1]))
